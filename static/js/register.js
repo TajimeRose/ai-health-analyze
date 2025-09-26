cf23f018b94538936ff7d54f6eb2722963090b93
Tun <aaoppo8979@gmail.com>
2025-09-26 15:30:17 +0700

    add a register file to code

---
+++ static/js/register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAIMvn1MWWUzNNr28Jy9ngf-yG4DElViiA",
+    authDomain: "login-aha-7e4ce.firebaseapp.com",
+    projectId: "login-aha-7e4ce",
+    storageBucket: "login-aha-7e4ce.firebasestorage.app",
+    messagingSenderId: "427985327773",
+    appId: "1:427985327773:web:e25560e962d7f788396674"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
